8b363acc94b3296bb8dee2b14c9483af71684413
SteveBaunfire <sfelizardo@baunfire.com>
2025-10-03 10:51:52 +0800

    add story card

---
+++ facebook/lib/widgets/story_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StorCard extends StatelessWidget {
+  const StorCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
